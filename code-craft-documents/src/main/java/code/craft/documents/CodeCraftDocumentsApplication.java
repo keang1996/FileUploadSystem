12ffdd17d531b33cc19011e6b09facd81f11c180
pongsakorn.akk <pongsakorn.akk@ascendcorp.com>
2024-02-19 08:53:31 +0700

    init documents

---
+++ code-craft-documents/src/main/java/code/craft/documents/CodeCraftDocumentsApplication.java
@@ -0,0 +1,13 @@
+package code.craft.documents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCraftDocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCraftDocumentsApplication.class, args);
+	}
+
+}
---
+++ code-craft-documents/src/test/java/code/craft/documents/CodeCraftDocumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package code.craft.documents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCraftDocumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
