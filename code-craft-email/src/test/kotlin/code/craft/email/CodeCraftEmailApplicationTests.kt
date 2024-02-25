1bf9d8c2b1832f37ce0e148657576d0a8e1b09c6
pongsakorn.akk <pongsakorn.akk@ascendcorp.com>
2024-02-25 23:09:28 +0700

    initial email service

---
+++ code-craft-email/src/test/kotlin/code/craft/email/CodeCraftEmailApplicationTests.kt
@@ -0,0 +1,13 @@
+package code.craft.email
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodeCraftEmailApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
